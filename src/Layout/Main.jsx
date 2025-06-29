692fdbdac2697a86cbff1bf0b9e07878f2f78186
monir995 <moniredt@gmail.com>
2025-06-29 10:26:27 +0600

    Second push - updated files

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
+import Navber from "../Pages/Shared/Navber/Navber";
 
 
 const Main = () => {
     return (
-        <div>
+        <div className="w-[1300px] mx-auto">
+            <Navber></Navber>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import router from './Routes/Routes';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
